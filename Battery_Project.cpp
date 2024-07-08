f997e00f3493fcb9c76d4a804a3949dbc278feb3
stenslae <141681972+stenslae@users.noreply.github.com>
2024-07-08 13:28:32 -0600

    Update Battery_Project.cpp

--- Battery_Project.cpp
+++ Battery_Project.cpp
@@ -40,7 +40,11 @@ class CircuitVariables {
         double voltage;
 	    double power;
 	    double wh;
+        double ah;
+        double current;
+        double mamp_hours = 0;
 	    double watt_hours=0;
+        double tot_volts = 0;
         string batt_name;
 	    string date_of_test;
 	    string id;
@@ -75,42 +79,61 @@ int main()
     Voltage_readings << "Battery Name:," << Battery.batt_name << "\n";
     Voltage_readings << "Resistor ID:," << Battery.id << "\n";
     Voltage_readings << "Date:," << Battery.date_of_test << "\n";
-    Voltage_readings << "Resistance:," << Battery.resistance << "\n";
-    Voltage_readings << "Total Watt-Hours:," << "\n\n";
-    Voltage_readings << "Minutes,Hours,Voltage (V), Power (W), Watt-Hours" << "\n";
+    Voltage_readings << "Load Resistance:," << Battery.resistance << "\n";
+    Voltage_readings << "Ave Volts:," << "\n";
+    Voltage_readings << "Total Watt-Hours:," << "\n";
+    Voltage_readings << "Total mAmp-Hours:," << "\n\n";
+    Voltage_readings << "Minutes,Hours,Voltage (V), Current(mA), Power (W), Watt-Hours, mAmp-Hours" << "\n";
 
     read_LJM.read();
 
     double init_voltage = Battery.voltage;
     int count = 0;
+    int avecount = 0;
+    int finalreads = 0;
 
     // checks to see if the battery is outputing below an arbitrary amount of watts so that recording stops once battery is dead
-    while (Battery.voltage > init_voltage/10.0)
-    {
+    while (finalreads < 20) {
         // updates values for data
         read_LJM.read();
 
         // takes the voltage and power and time
 	    Battery.time = (count*(60.0/Battery.reads_per_minute))/60.0;
         Battery.voltage = read_LJM.values[0]-read_LJM.values[1];
-	    Battery.power = (Battery.voltage*Battery.voltage)/Battery.resistance;	
-	    //calculate total watt hours
+        Battery.current = 1000 * Battery.voltage / Battery.resistance;
+	    Battery.power = (Battery.voltage*Battery.voltage)/Battery.resistance;
+
+        //sum up the voltage for averaging
+        if (Battery.voltage > init_voltage / 10.0) {
+            avecount += 1;
+            Battery.tot_volts += Battery.voltage;
+        }
+        //calculate total watt hours and mamp hours
 	    if((count != 0) && (Battery.voltage > init_voltage / 10.0)){
 		    Battery.wh = Battery.power*((Battery.time/60)-(((count-1)*(60.0/Battery.reads_per_minute))/60.0/60.0));
+            Battery.ah = Battery.current * ((Battery.time / 60) - (((count - 1) * (60.0 / Battery.reads_per_minute)) / 60.0 / 60.0));
 	    }else{
 		    Battery.wh = 0;
+            Battery.ah = 0;
 	    }
 	    Battery.watt_hours = Battery.watt_hours + Battery.wh;
+        Battery.mamp_hours = Battery.mamp_hours + Battery.ah;
 
         // adds data to data file
-        Voltage_readings << Battery.time << "," << Battery.time/60 << "," << Battery.voltage << "," << Battery.power << "," << Battery.wh << "\n";
+        Voltage_readings << Battery.time << "," << Battery.time/60 << "," << Battery.voltage << "," << Battery.current << "," << Battery.power << "," << Battery.wh << "," << Battery.ah << "\n";
         cout << Battery.voltage << "\n";
-	
+
+        if (Battery.voltage < init_voltage / 10.0) {
+            finalreads += 1;
+        }
 	    count += 1;
         // waits until next reading is needed
         Sleep(1000*(60.0/Battery.reads_per_minute));
 	
     }
+
+    //Calculate the average voltage
+    Battery.tot_volts = Battery.tot_volts / avecount;
 
     // Close the voltage readings file
     Voltage_readings.close();
@@ -123,7 +146,7 @@ int main()
 
     // Create new file
     ostringstream fileNameStream;
-    fileNameStream << "../" << batt_name_sanitized << "_test_" << date_of_test_sanitized << ".csv";
+    fileNameStream << "../" << batt_name_sanitized << "_batteryTest_" << date_of_test_sanitized << ".csv";
     string fileName = fileNameStream.str();
     ofstream Final_file(fileName);
 
@@ -135,11 +158,17 @@ int main()
     while (getline(Voltage_readings_in, line)){
     	if(line.find("Total Watt-Hours:,") != string::npos){
 	        Final_file << "Total Watt-Hours:," << Battery.watt_hours << "\n";
-	    }else{
+	    }
+        else if (line.find("Total mAmp-Hours:,") != string::npos) {
+            Final_file << "Total mAmp-Hours:," << Battery.mamp_hours << "\n";
+        }
+        else if (line.find("Ave Volts:,") != string::npos) {
+            Final_file << "Ave Volts:," << Battery.tot_volts << "\n";
+        }
+        else{
 	        Final_file << line << "\n";
 	    }
     }
-
     
     // closes files and devices
     Voltage_readings.close();
